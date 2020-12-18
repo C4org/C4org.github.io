d0fdf630c0fbb9bbb72aa6cf89b9f76dacfa52da
Kandasamy Chokkalingam <samchoks@hotmail.com>
2020-12-17 19:02:09 -0500

    Update count.js

--- js/count.js
+++ js/count.js
@@ -4,23 +4,27 @@ let time_start = 2200;
 let time_start_2 = 1500;
 let past_width = null;
 let trigger_width = 767;
+const count1 = 7;
+const count2 = 195;
+const count3 = 2;
+const count4 = 2112;
 
-let demo = new CountUp('countUp1', 7, { duration: 3 });
+let demo = new CountUp('countUp1', count1, { duration: 3 });
 setTimeout(function () {
     demo.start();
 }, time_start);
 
-let demo1 = new CountUp('countUp2', 195, { duration: 3 });
+let demo1 = new CountUp('countUp2', count2, { duration: 3 });
 setTimeout(function () {
     demo1.start();
 }, time_start);
 
-let demo2 = new CountUp('countUp3', 2, { duration: 3 });
+let demo2 = new CountUp('countUp3', count3, { duration: 3 });
 setTimeout(function () {
     demo2.start();
 }, time_start);
 
-let demo3 = new CountUp('countUp4', 2112, {
+let demo3 = new CountUp('countUp4', count4, {
     prefix: '$',
 });
 setTimeout(function () {
@@ -31,43 +35,43 @@ update();
 
 function update() {
     if ((document.documentElement.clientWidth > trigger_width > past_width) || (document.documentElement.clientWidth < trigger_width < past_width) || past_width == null) {
-        let demo = new CountUp('countUp1', 7, { duration: 3 });
+        let demo = new CountUp('countUp1', count1, { duration: 3 });
         setTimeout(function () {
             demo.start();
         }, time_start);
 
-        let demo1 = new CountUp('countUp2', 195, { duration: 3 });
+        let demo1 = new CountUp('countUp2', count2, { duration: 3 });
         setTimeout(function () {
             demo1.start();
         }, time_start);
 
-        let demo2 = new CountUp('countUp3', 2, { duration: 3 });
+        let demo2 = new CountUp('countUp3', count3, { duration: 3 });
         setTimeout(function () {
             demo2.start();
         }, time_start);
 
-        let demo3 = new CountUp('countUp4', 2112, {
+        let demo3 = new CountUp('countUp4', count4, {
             prefix: '$',
         });
         setTimeout(function () {
             demo3.start();
         }, time_start);
-        let demo5 = new CountUp('countUp5',6, { duration: 3 });
+        let demo5 = new CountUp('countUp5',count1, { duration: 3 });
         setTimeout(function () {
             demo5.start();
         }, time_start_2);
 
-        let demo6 = new CountUp('countUp6', 167, { duration: 3 });
+        let demo6 = new CountUp('countUp6', count2, { duration: 3 });
         setTimeout(function () {
             demo6.start();
         }, time_start_2);
 
-        let demo7 = new CountUp('countUp7', 2, { duration: 3 });
+        let demo7 = new CountUp('countUp7', count3, { duration: 3 });
         setTimeout(function () {
             demo7.start();
         }, time_start_2);
 
-        let demo8 = new CountUp('countUp8', 2112, {
+        let demo8 = new CountUp('countUp8', count4, {
             prefix: '$',
         });
         setTimeout(function () {
